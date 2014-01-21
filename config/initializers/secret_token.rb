2ed2363579dbe62732d472b2d4a33def5da99062
kwaghray <kwaghray@kwaghray-Aspire-5532.(none)>
2014-01-21 22:00:20 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kunal::Application.config.secret_token = '1beace3f6e20af367c1b13d192396be1b2c239a9edfe55333cc5780531072096ab69aea814dc4abad39e3f4909a5288a86423fc70a9004e3b03b964bcec47c6f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Kunal::Application.config.session_store :cookie_store, key: '_kunal_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Kunal::Application.config.session_store :active_record_store
